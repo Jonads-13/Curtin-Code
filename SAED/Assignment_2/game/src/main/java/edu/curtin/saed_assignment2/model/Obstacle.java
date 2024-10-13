56008008e7b70cb0464fe784e3f752551c0fd197
Jake <jjonas@bigpond.com>
2024-10-13 14:41:07 +0800

    switched from array to list

--- SAED/Assignment_2/game/src/main/java/edu/curtin/saed_assignment2/model/Obstacle.java
+++ SAED/Assignment_2/game/src/main/java/edu/curtin/saed_assignment2/model/Obstacle.java
@@ -1,20 +1,29 @@
 package edu.curtin.saed_assignment2.model;
 
+import java.util.List;
+
 public class Obstacle extends Cell {
 
-    private String[] itemRequirements;
+    private List<String> itemRequirements;
 
-    public Obstacle(int row, int col, String[] requirements) {
+    public Obstacle(int row, int col, List<String> requirements) {
         super(row, col);
         this.itemRequirements = requirements;
         label = " O ";
     }
 
-    public String[] getItemRequirements() {
+    public List<String> getItemRequirements() {
         return itemRequirements;
     }
 
-    public void setItemRequirements(String[] requirements) {
+    public void setItemRequirements(List<String> requirements) {
         this.itemRequirements = requirements;
+    }
+
+    public void displayRequirements() {
+        System.out.println("Reqiured to pass this obstacle: ");
+        for (String requirement : itemRequirements) {
+            System.out.println(requirement);
+        }
     }
 }
